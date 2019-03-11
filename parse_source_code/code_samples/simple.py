9d0b806b4488d0ecfd9d41ff2b2e7cecb39cd370
Natalie Bogda <nbogda@vols.utk.edu>
2019-03-11 01:20:21 -0400

    Converted files to vectors

---
+++ parse_source_code/code_samples/simple.py
@@ -0,0 +1,5 @@
+def sum_num(a, b):
+    return a + b
+
+
+print(sum_num(2, 3))
--- parse_source_code/parse_source.py
+++ parse_source_code/parse_source.py
@@ -1,26 +1,51 @@
 import bblfsh
 import sys
-from collections import defaultdict
 import re
+from gensim.models import Doc2Vec
+import os
+from gensim.models.doc2vec import TaggedDocument
 
 #connect to babelfish client
 client = bblfsh.BblfshClient("0.0.0.0:9432")
-ast = client.parse("code_samples/%s" % sys.argv[1]).uast
 
-#uncomment this to look at the AST produced
-print(ast)
+#walking through all source files of a certain type
+def walk(file_type):
 
-#put nodes on stack
-stack = []
-for node in bblfsh.iterator(ast, bblfsh.TreeOrder.PRE_ORDER):
-    stack.append(node)
+    type_list = []
+    for root, dirnames, filenames in os.walk("code_samples"):
+        for filename in filenames:
+            #check file extension here
+            if filename.endswith(".%s" % file_type):
+                path = os.path.join(root, filename)
+                ast = client.parse(path).uast
+                ast = str(ast)
+                internal_types = re.findall('internal_type: "(.*)"', ast)
+                type_list.append(internal_types)
 
-#traverse stack, collect internal types of nodes
-result = defaultdict(int)
-while stack:
-    node = stack.pop()
-    stack.extend(node.children)
-    node = str(node)
-    #token we are looking for
-    internal_type = re.search('internal_type: "(.*)"', node).group(1)
+    return type_list
 
+def convert2vec(docs):
+
+    conversion = []
+    #convert to tagged document
+    for d in docs:
+        conversion.append(TaggedDocument(words=d, tags=["test"]))
+    model = Doc2Vec(dm=0, vector_size=300, window=5)
+    model.build_vocab(conversion)
+    model.train(conversion, total_examples=len(docs), epochs=30)
+    labels = []
+    values = []
+    for c in conversion:
+        labels.append(c.tags[0])
+        values.append(model.infer_vector(c.words, steps=20))
+
+    return labels, values
+    
+
+
+if __name__ == "__main__":
+
+    #usage: py or java
+    file_type = str(sys.argv[1])
+    type_list = walk(file_type)
+    labels, values = convert2vec(type_list)
